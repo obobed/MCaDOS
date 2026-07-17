0dda4f542b85cec538cab1dcf4047137d6ac412e
DaBlower <113282214+obobed@users.noreply.github.com>
2026-07-17 19:11:13 +1000

    patterndetector class

---
+++ main.py
@@ -0,0 +1,30 @@
+import time
+
+# we need vars for thresholds
+TAP_MAX = 0.35 # press and release combo before this time = a tap
+GAP_MAX = 0.35 # silence longer than this = a gap
+
+class PatternDetector:
+    def __init__(self, on_pattern) -> None: # on_pattern being the handler function
+        self.on_pattern = on_pattern 
+        self.sequence = ""
+        self.press_time = None
+        self.last_event_time = None
+        
+    def press(self):
+        self.press_time = time.monotonic()
+
+    def release(self):
+        if self.press_time is None:
+            return # maybe some debug logging here?
+        duration = time.monotonic() - self.press_time
+        self.sequence += "H" if duration > TAP_MAX else "T" # h & t are hold and tap respectively if you're slow like me :sob:
+        self.last_event_time = time.monotonic()
+        self.press_time = None
+    
+    def check_timeout(self): # call every like 50ms from a loop or timer, basically checks if the sequence has timed out
+        if self.sequence and self.last_event_time:
+            if time.monotonic() - self.last_event_time > GAP_MAX:
+                self.on_pattern(self.sequence)
+                self.sequence = ""
+                self.last_event_time = None
