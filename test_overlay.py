d264e213aff64516cf208cb9c3b22b5ed79dad16
DaBlower <113282214+obobed@users.noreply.github.com>
2026-07-18 13:25:00 +1000

    slightly cleanup working dir

--- test_overlay.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from overlay import Overlay
-
-app = QApplication(sys.argv)
-overlay = Overlay()
-overlay.update_text("test")
-sys.exit(app.exec())
